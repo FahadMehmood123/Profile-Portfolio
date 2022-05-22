bab595848a85f8b01bf3d0b95df2e47175deabcd
FahadMehmood123 <85797986+FahadMehmood123@users.noreply.github.com>
2022-05-22 10:05:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ import Experience from "./components/Experience/Experience";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-// import { useContext } from "react";
-// import { themeContext } from "./Context";
+
 function App() {
-  // const theme = useContext(themeContext);
-  // const darkMode = theme.state.darkMode;
   return (
-    <div
-      className="App"
-      // style={{
-      //   background: darkMode ? "black" : "",
-      //   color: darkMode ? "white" : "",
-      // }}
-    >
+    <div className="App">
       <Navbar />
       <Intro />
       <Experience />
